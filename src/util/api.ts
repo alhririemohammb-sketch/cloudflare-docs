906f4cc8752b718bcc7eb727e5a72ddc32a348dc
alhririemohammb-sketch <alhririemohammb@gmail.com>
2026-01-29 16:58:23 +0400

    Update api.ts

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,18 +1,20 @@
-import SwaggerParser from "@apidevtools/swagger-parser";
-import type { OpenAPI } from "openapi-types";
+<br />
 
-const COMMIT = "6b852f9040e6f578aa91b159af2f933527465f72";
-let schema: OpenAPI.Document | undefined;
+The following are links to previously published[*Additional Terms of Service*](https://ai.google.dev/terms)documents:
 
-export const getSchema = async () => {
-	if (!schema) {
-		const response = await fetch(
-			`https://gh-code.developers.cloudflare.com/cloudflare/api-schemas/${COMMIT}/openapi.json`,
-		);
-		const obj = await response.json();
-
-		schema = await SwaggerParser.dereference(obj);
-	}
-
-	return schema;
-};
+- [November 20, 2025](https://ai.google.dev/gemini-api/terms-archive/terms_11_20_25)
+- [October 17, 2025](https://ai.google.dev/gemini-api/terms-archive/terms_10_17_25)
+- [October 7, 2025](https://ai.google.dev/gemini-api/terms-archive/terms_10_07_25)
+- [September 25, 2025](https://ai.google.dev/gemini-api/terms-archive/terms_09_25_25)
+- [May 20, 2025](https://ai.google.dev/gemini-api/terms-archive/terms_05_20_25)
+- [April 3, 2025](https://ai.google.dev/gemini-api/terms-archive/terms_04_03_25)
+- [February 5, 2025](https://ai.google.dev/gemini-api/terms-archive/terms_02_05_25)
+- [December 20, 2024](https://ai.google.dev/gemini-api/terms-archive/terms_12_20_24)
+- [May 14, 2024](https://ai.google.dev/gemini-api/terms-archive/terms_05_14_24)
+- [May 2, 2024](https://ai.google.dev/gemini-api/terms-archive/terms_05_02_24)
+- [March 20, 2024](https://ai.google.dev/gemini-api/terms-archive/terms_03_20_24)
+- [February 15, 2024](https://ai.google.dev/gemini-api/terms-archive/terms_02_15_24)
+- [December 13, 2023](https://ai.google.dev/gemini-api/terms-archive/terms_12_13_23)
+- [September 20, 2023](https://ai.google.dev/gemini-api/terms-archive/terms_09_20_23)
+- [August 28, 2023](https://ai.google.dev/gemini-api/terms-archive/terms_08_28_23)
+- [May 10, 2023](https://ai.google.dev/gemini-api/terms-archive/terms_05_10_23)
